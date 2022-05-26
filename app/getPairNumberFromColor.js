e13aec819c3107ae65195557a9d00f42850b5e22
Zakiya <31302217+kmzakiya@users.noreply.github.com>
2022-05-26 21:53:39 +0530

    removing duplicate code

--- app/getPairNumberFromColor.js
+++ app/getPairNumberFromColor.js
@@ -1,21 +1,8 @@
 import { MAJOR_COLOR_NAMES, MINOR_COLOR_NAMES } from "./colorNames.constants.js";
 
 export function getPairNumberFromColor(pair) {
-  let majorColorIndex = -1;
-  for (let i = 0; i < MAJOR_COLOR_NAMES.length; i++) {
-    if (MAJOR_COLOR_NAMES[i] == pair.majorColor) {
-      majorColorIndex = i;
-      break;
-    }
-  }
-
-  let minorColorIndex = -1;
-  for (let i = 0; i < MINOR_COLOR_NAMES.length; i++) {
-    if (MINOR_COLOR_NAMES[i] == pair.minorColor) {
-      minorColorIndex = i;
-      break;
-    }
-  }
+  const majorColorIndex = getColorIndex(pair.majorColor, MAJOR_COLOR_NAMES);
+  const minorColorIndex = getColorIndex(pair.minorColor, MINOR_COLOR_NAMES);
 
   if (majorColorIndex == -1 || minorColorIndex == -1) {
     throw `Unknown Colors:${pair.toString()}`;
@@ -24,3 +11,13 @@ export function getPairNumberFromColor(pair) {
   return majorColorIndex * MINOR_COLOR_NAMES.length + (minorColorIndex + 1);
 }
 
+function getColorIndex(color, colourNames) {
+  let index = -1;
+  for (let i = 0; i < colourNames.length; i++) {
+    if (colourNames[i] == color) {
+      index = i;
+      break;
+    }
+  }
+  return index;
+}
